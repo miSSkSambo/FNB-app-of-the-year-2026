683b0a1ec5cc7879b5a04be74927c18247fb961b
k sambo <katlego2390@gmail.com>
2026-07-15 14:34:14 +0200

    Add concert ticket booking script

---
+++ lesson 1/The challenge 1/concert_ticket_booker.py
@@ -0,0 +1,10 @@
+# Concert Ticket Booker
+
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Ask the user for the artist they want to see
+artist = input("Which band or artist would you like to see? ")
+
+# Display a confirmation message
+print(f"\nHey {name}! Your tickets to see {artist} are booked successfully!")
